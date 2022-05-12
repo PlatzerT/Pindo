f2e2ce859a137809c337d79079d76b7cd756e1fa
Max <klacklmaximilian@gmail.com>
2022-05-12 09:32:37 +0200

    example push notification

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import * as Notification from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+Notification.setNotificationHandler({
+    handleNotification: async () => {
+        return {
+            shouldShowAlert: true,
+            shouldPlaySound: true
+        };
+    }
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    //Exectute at the launch of app for ios
+    useEffect(() => {
+
+        Permissions.getAsync(Permissions.NOTIFICATIONS)
+            .then((statusObj) => {
+                if (statusObj.status !== 'granted') {
+                    return Permissions.askAsync(Permissions.NOTIFICATIONS);
+                }
+                return statusObj;
+            }).then(statusObj => {
+            if (statusObj.status !== 'granted') {
+                alert('Notifications will be unavailable now');
+                return;
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        //When app is closed
+        const backgroundSubscription = Notification.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+        //When the app is open
+        const foregroundSubscription = Notification.addNotificationReceivedListener(notification => {
+            console.log(notification);
+        });
+
+        return () => {
+            backgroundSubscription.remove();
+            foregroundSubscription.remove();
+        }
+    }, []);
+    //=======================================================
+
+    //Trigger Function Called by click of the button to
+    //trigger notification
+
+    //=======================================================
+    const triggerNotification = () => {
+        Notification.scheduleNotificationAsync({
+            content: {
+                title: "My First Notification",
+                body: "Local Notification to be sent"
+            },
+            trigger: {
+                seconds: 1
+            }
+        });
+    }
+    return (
+        <View style={styles.container}>
+            <Button title="Send Notification" onPress={triggerNotification} />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
