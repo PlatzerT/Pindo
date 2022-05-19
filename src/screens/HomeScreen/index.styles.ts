2aa8e93284634c4bf3b7f7c8e2ad4edd68226764
Thomas Platzer <thomas.platzer@htl-wels.at>
2022-05-19 08:41:29 +0200

    .

---
+++ src/screens/HomeScreen/index.styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    height: "100vh",
+  },
+});
+
+export default styles;
---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React from "react";
+import styles from "./index.styles";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>ooook</Text>
+    </View>
+  );
+}
