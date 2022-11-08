20a0dde3a2a7e5d2a7f312ab2bd22747299da398
PlatzerT <thpl.school@gmail.com>
2022-11-08 12:29:39 +0100

    .

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
-import 'react-native-gesture-handler';
-import React, {useEffect} from "react";
+import "react-native-gesture-handler";
+import React, { useEffect } from "react";
 import HomeScreen from "./src/screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EditScreen from "./src/screens/EditScreen";
 import TodosProvider from "./src/context/TodosProvider";
+import {
+  TapGestureHandler,
+  RotationGestureHandler,
+} from "react-native-gesture-handler";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <TodosProvider>
+    <TapGestureHandler>
+      <RotationGestureHandler>
+        <TodosProvider>
           <NavigationContainer>
-              <Stack.Navigator screenOptions={{ headerShown: false }}>
-                  <Stack.Screen name="Home" component={HomeScreen} />
-                  <Stack.Screen name="Edit" component={EditScreen} />
-              </Stack.Navigator>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Edit" component={EditScreen} />
+            </Stack.Navigator>
           </NavigationContainer>
-      </TodosProvider>
+        </TodosProvider>
+      </RotationGestureHandler>
+    </TapGestureHandler>
   );
 }
--- src/screens/EditScreen/index.tsx
+++ src/screens/EditScreen/index.tsx
@@ -1,248 +1,284 @@
-import React, {useEffect, useRef, useState} from "react";
-import {LogBox, Platform, Switch, Text, TextInput, TouchableHighlight, TouchableOpacity, View} from "react-native";
-import {colors, sharedStyles} from "../../styles/base";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  LogBox,
+  Platform,
+  Switch,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { colors, sharedStyles } from "../../styles/base";
 import styles from "./index.styles";
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from "react-native-vector-icons/Feather";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
-import {ITodo} from "../../models/ITodo";
-import {useTodos} from "../../context/TodosProvider";
-import {formatDate} from "../../utils/dateUtils";
-import * as Notifications from 'expo-notifications';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel,
+} from "react-native-simple-radio-button";
+import { ITodo } from "../../models/ITodo";
+import { useTodos } from "../../context/TodosProvider";
+import { formatDate } from "../../utils/dateUtils";
+import * as Notifications from "expo-notifications";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Constants from "expo-constants";
-import {scheduleTodoNotification} from "../../services/notification-service";
+import { scheduleTodoNotification } from "../../services/notification-service";
 
 interface IProps {
-    navigation: any;
-    route: any;
+  navigation: any;
+  route: any;
 }
 
 const priorities = [
-    {
-        label: "Low",
-        value: "low",
-    },
-    {
-        label: "Medium",
-        value: "medium",
-    },
-    {
-        label: "High",
-        value: "high",
-    },
+  {
+    label: "Low",
+    value: "low",
+  },
+  {
+    label: "Medium",
+    value: "medium",
+  },
+  {
+    label: "High",
+    value: "high",
+  },
 ];
 
 function getButtonColorByIndex(i: number) {
-    let btColor;
-    switch (i) {
-        case 0:
-            btColor = colors.pLow;
-            break;
-        case 1:
-            btColor = colors.pMedium;
-            break;
-        case 2:
-            btColor = colors.pHigh;
-            break;
-    }
-    return btColor
+  let btColor;
+  switch (i) {
+    case 0:
+      btColor = colors.pLow;
+      break;
+    case 1:
+      btColor = colors.pMedium;
+      break;
+    case 2:
+      btColor = colors.pHigh;
+      break;
+  }
+  return btColor;
 }
 
 Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: true,
-        shouldSetBadge: true,
-    })
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
 });
 
-export default function EditScreen({navigation, route}: IProps) {
-    const [notification, setNotification] = useState(false);
-    const notificationListener = useRef();
-    const responseListener = useRef();
+export default function EditScreen({ navigation, route }: IProps) {
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
-    useEffect(() => {
-        const getPermission = async () => {
-            if (Constants.isDevice) {
-                const {status: existingStatus} = await Notifications.getPermissionsAsync();
-                let finalStatus = existingStatus;
-                if (existingStatus !== 'granted') {
-                    const {status} = await Notifications.requestPermissionsAsync();
-                    finalStatus = status;
-                }
-                if (finalStatus !== 'granted') {
-                    alert('Enable push notifications to use the app!');
-                    await AsyncStorage.setItem('expopushtoken', "");
-                    return;
-                }
-                const token = (await Notifications.getExpoPushTokenAsync()).data;
-                await AsyncStorage.setItem('expopushtoken', token);
-            } else {
-                alert('Must use physical device for Push Notifications');
-            }
-
-            if (Platform.OS === 'android') {
-                await Notifications.setNotificationChannelAsync('todos', {
-                    name: 'Todos',
-                    importance: Notifications.AndroidImportance.MAX,
-                    showBadge: true,
-                    vibrationPattern: [0, 250, 250, 250],
-                    lightColor: colors.primary,
-                });
-            }
+  useEffect(() => {
+    const getPermission = async () => {
+      if (Constants.isDevice) {
+        const { status: existingStatus } =
+          await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
         }
-
-        getPermission();
-
-        // @ts-ignore
-        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-            // @ts-ignore
-            setNotification(notification);
-        });
-
-        // @ts-ignore
-        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-        });
-
-        return () => {
-            Notifications.removeNotificationSubscription(notificationListener.current);
-            Notifications.removeNotificationSubscription(responseListener.current);
-        };
-    }, []);
-
-    const {todo} = route.params;
-    const {saveTodo} = useTodos();
-    LogBox.ignoreLogs([
-        'Non-serializable values were found in the navigation state',
-    ]);
-
-    const [date, setDate] = useState(todo.date ? todo.date : new Date());
-    const [mode, setMode] = useState('date');
-    const [show, setShow] = useState(false);
-    const textRef = useRef();
-    const [text, setText] = useState(todo.text);
-    const [continuous, setContinuous] = useState(todo.date == null);
-    const toggleSwitch = () => {
-        setContinuous(previousState => !previousState);
-    }
-    const [priority, setPriority] = useState(todo.priority);
-
-    function save() {
-
-        if (text.length === 0 || text === "") {
-            // @ts-ignore
-            textRef.current.focus();
-        } else {
-            const t: ITodo = {
-                id: todo.id,
-                text: text,
-                date: !continuous ? date : null,
-                priority: priority,
-                isDeleted: todo.isDeleted,
-            }
-
-            saveTodo(t).then(async () => {
-                await scheduleTodoNotification(t);
-                navigation.navigate("Home");
-            })
+        if (finalStatus !== "granted") {
+          alert("Enable push notifications to use the app!");
+          await AsyncStorage.setItem("expopushtoken", "");
+          return;
         }
-    }
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        await AsyncStorage.setItem("expopushtoken", token);
+      } else {
+        alert("Must use physical device for Push Notifications");
+      }
 
-    function showMode(currentMode) {
-        setShow(true);
-        setMode(currentMode);
-    }
+      if (Platform.OS === "android") {
+        await Notifications.setNotificationChannelAsync("todos", {
+          name: "Todos",
+          importance: Notifications.AndroidImportance.MAX,
+          showBadge: true,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: colors.primary,
+        });
+      }
+    };
 
-    function onChangeDate(e, selectedDate) {
-        const currentDate = selectedDate || date;
-        setShow(Platform.OS === 'ios');
-        setDate(currentDate);
-    }
+    getPermission();
 
     // @ts-ignore
-    return <View style={sharedStyles.screenBackground}>
-        <View style={styles.contentSection}>
-            <Text style={styles.label}>Text</Text>
-            <TextInput
-                style={styles.textInput}
-                value={text}
-                ref={textRef}
-                onChangeText={setText}
-                selectTextOnFocus={true}
-                placeholder={"Text here"}/>
-            <View style={styles.s2}>
-                <View>
-                    <Text style={styles.label}>Deadline</Text>
-                    {continuous ? <Text>-</Text> :
-                        <TouchableOpacity activeOpacity={0.4} style={styles.calendarButton}
-                                          onPress={() => showMode('date')}><Text>{formatDate(date)}</Text></TouchableOpacity>}
-                    {show && <DateTimePicker
-                        testID={'dateTimePicker'}
-                        value={date}
-                        // @ts-ignore
-                        mode={mode}
-                        is24Hour={true}
-                        display={"default"}
-                        onChange={onChangeDate}
-                    />}
-                </View>
-                <View>
-                    <Text style={styles.label}>Continuous</Text>
-                    <Switch
-                        style={styles.showContinuouslySwitch}
-                        trackColor={{false: '#767577', true: '#D2D6FF'}}
-                        thumbColor={continuous ? '#4b58f3' : '#f4f3f4'}
-                        ios_backgroundColor="#fff"
-                        onValueChange={toggleSwitch}
-                        value={continuous}/>
-                </View>
-            </View>
-            <View>
-                <Text style={styles.priorityLabel}>Priority</Text>
-                <RadioForm
-                    formHorizontal={true}
-                    animation={true}
-                    style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}
-                >
-                    {
-                        priorities.map((obj, i) => {
-                            const btColor = getButtonColorByIndex(i);
-                            return <RadioButton labelHorizontal={true} key={i}>
-                                <RadioButtonInput
-                                    obj={obj}
-                                    index={i}
-                                    isSelected={obj.value === priority}
-                                    onPress={(value) => setPriority(value)}
-                                    borderWidth={2}
-                                    buttonInnerColor={btColor}
-                                    buttonOuterColor={btColor}
-                                    buttonSize={14}
-                                    buttonOuterSize={24}
-                                    buttonStyle={{}}
-                                />
-                                <RadioButtonLabel
-                                    obj={obj}
-                                    index={i}
-                                    labelHorizontal={true}
-                                    onPress={(value) => setPriority(value)}
-                                    labelStyle={{color: '#000'}}
-                                    labelWrapStyle={{}}
-                                />
-                            </RadioButton>
-                        })
-                    }
-                </RadioForm>
-            </View>
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        // @ts-ignore
+        setNotification(notification);
+      });
+
+    // @ts-ignore
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {});
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  const { todo } = route.params;
+  const { saveTodo } = useTodos();
+  LogBox.ignoreLogs([
+    "Non-serializable values were found in the navigation state",
+  ]);
+
+  const [date, setDate] = useState(todo.date ? todo.date : new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+  const textRef = useRef();
+  const [text, setText] = useState(todo.text);
+  const [continuous, setContinuous] = useState(todo.date == null);
+  const toggleSwitch = () => {
+    setContinuous((previousState) => !previousState);
+  };
+  const [priority, setPriority] = useState(todo.priority);
+
+  function save() {
+    if (text.length === 0 || text === "") {
+      // @ts-ignore
+      textRef.current.focus();
+    } else {
+      const t: ITodo = {
+        id: todo.id,
+        text: text,
+        date: !continuous ? date : null,
+        priority: priority,
+        isDeleted: todo.isDeleted,
+      };
+
+      saveTodo(t).then(async () => {
+        await scheduleTodoNotification(t);
+        navigation.navigate("Home");
+      });
+    }
+  }
+
+  function showMode(currentMode) {
+    setShow(true);
+    setMode(currentMode);
+  }
+
+  function onChangeDate(e, selectedDate) {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  }
+
+  // @ts-ignore
+  return (
+    <View style={sharedStyles.screenBackground}>
+      <View style={styles.contentSection}>
+        <Text style={styles.label}>Text</Text>
+        <TextInput
+          style={styles.textInput}
+          value={text}
+          ref={textRef}
+          onChangeText={setText}
+          selectTextOnFocus={true}
+          multiline
+          placeholder={"Text here"}
+        />
+        <View style={styles.s2}>
+          <View>
+            <Text style={styles.label}>Deadline</Text>
+            {continuous ? (
+              <Text>-</Text>
+            ) : (
+              <TouchableOpacity
+                activeOpacity={0.4}
+                style={styles.calendarButton}
+                onPress={() => showMode("date")}
+              >
+                <Text>{formatDate(date)}</Text>
+              </TouchableOpacity>
+            )}
+            {show && (
+              <DateTimePicker
+                testID={"dateTimePicker"}
+                value={date}
+                // @ts-ignore
+                mode={mode}
+                is24Hour={true}
+                display={"default"}
+                onChange={onChangeDate}
+              />
+            )}
+          </View>
+          <View>
+            <Text style={styles.label}>Continuous</Text>
+            <Switch
+              style={styles.showContinuouslySwitch}
+              trackColor={{ false: "#767577", true: "#D2D6FF" }}
+              thumbColor={continuous ? "#4b58f3" : "#f4f3f4"}
+              ios_backgroundColor="#fff"
+              onValueChange={toggleSwitch}
+              value={continuous}
+            />
+          </View>
         </View>
-        <TouchableHighlight
-            style={styles.actionSection}
-            underlayColor={colors.primary}
-            onPress={() => save()}>
-            <View style={styles.center}>
-                <Icon name="bookmark" style={styles.icon} size={24}/>
-                <Text style={styles.saveText}>Tap to save</Text>
-            </View>
-        </TouchableHighlight>
-    </View>;
+        <View>
+          <Text style={styles.priorityLabel}>Priority</Text>
+          <RadioForm
+            formHorizontal={true}
+            animation={true}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            {priorities.map((obj, i) => {
+              const btColor = getButtonColorByIndex(i);
+              return (
+                <RadioButton labelHorizontal={true} key={i}>
+                  <RadioButtonInput
+                    obj={obj}
+                    index={i}
+                    isSelected={obj.value === priority}
+                    onPress={(value) => setPriority(value)}
+                    borderWidth={2}
+                    buttonInnerColor={btColor}
+                    buttonOuterColor={btColor}
+                    buttonSize={14}
+                    buttonOuterSize={24}
+                    buttonStyle={{}}
+                  />
+                  <RadioButtonLabel
+                    obj={obj}
+                    index={i}
+                    labelHorizontal={true}
+                    onPress={(value) => setPriority(value)}
+                    labelStyle={{ color: "#000" }}
+                    labelWrapStyle={{}}
+                  />
+                </RadioButton>
+              );
+            })}
+          </RadioForm>
+        </View>
+      </View>
+      <TouchableHighlight
+        style={styles.actionSection}
+        underlayColor={colors.primary}
+        onPress={() => save()}
+      >
+        <View style={styles.center}>
+          <Icon name="bookmark" style={styles.icon} size={24} />
+          <Text style={styles.saveText}>Tap to save</Text>
+        </View>
+      </TouchableHighlight>
+    </View>
+  );
 }
